4dae633d66b120c310f5748eabd03396be20771d
schultzhenry <natalie.schultz-henry@yale.edu>
2018-08-11 13:15:49 -0400

    handling touch on analogies

--- home.js
+++ home.js
@@ -63,9 +63,8 @@ $(document).ready(function() {
   };
 
   console.log(window.location.href);
-  if (window.location.href == 'schultzhenry.com' ||
-      window.location.href == 'http://schultzhenry.com' ||
-      window.location.href == 'https://schultzhenry.com' ||
+  if (window.location.href == 'http://schultzhenry.com/' ||
+      window.location.href == 'https://schultzhenry.com/' ||
       window.location.href == 'https://www.schultzhenry.com/#about' ||
       window.location.href == 'file:///Users/natalieschultz-henry/sites/schultzhenry.github.io/index.html') {
     console.log('Loading about page.');
