436c3fd71fe23c28ceb3e0ec147f71353848d40b
schultzhenry <natalie.schultz-henry@yale.edu>
2018-08-15 14:09:30 -0400

    nav

--- home.js
+++ home.js
@@ -57,8 +57,8 @@ $(document).ready(function() {
   $('#info').append(infoOpen);
 
   $('.navIcon').css({
-    'width':'44px',
-    'height':'44px'
+    'width':'40px',
+    'height':'40px'
   });
 
   $('#menu').click(function() {
