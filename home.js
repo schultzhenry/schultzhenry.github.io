3bde72289edf95dd791be44649bc0430950847cc
schultzhenry <natalie.schultz-henry@yale.edu>
2018-08-15 13:56:07 -0400

    nav

--- home.js
+++ home.js
@@ -515,64 +515,59 @@ $(document).ready(function() {
     $('#content').removeAttr('style');
     $('body').removeAttr('style');
 
-    $('.contactButton').click(function() {
+    $('#content').css({
+      'position': 'fixed',
+      'top': '0px',
+      'left': '0px',
+      'width': '100vw',
+      'height': '100vh',
+      'font-size': '26pt',
+      'color':'navy',
+      'text-align':'center'
+    });
+    $('#content').append(
+      '<a id="contact" ' +
+      'href="mailto:natalie.schultz-henry@yale.edu">' +
+      'Send me a note!' +
+      '</a>');
+    $('#contact').css({
+      'position':'absolute',
+      'left':'50%',
+      'top':'50%',
+      'margin':'auto',
+      'max-width':'100%',
+      'max-height':'100%',
+      'overflow':'auto',
+      'transform': 'translate(-50%, -50%)',
+      'font-size': '26pt',
+      'color':'MidnightBlue',
+      'text-decoration':'none',
+    });
 
-      $('#content').empty();
-      $('#content').removeAttr('style');
-      $('#content').css({
-        'position': 'fixed',
-        'top': '0px',
-        'left': '0px',
-        'width': '100vw',
-        'height': '100vh',
-        'font-size': '26pt',
-        'color':'navy',
-        'text-align':'center'
-      });
-      $('#content').append(
-        '<a id="contact" ' +
-        'href="mailto:natalie.schultz-henry@yale.edu">' +
-        'Send me a note!' +
-        '</a>');
-      $('#contact').css({
+    // When mouse hovers on contact button
+    $('#contact').mouseenter(function() {
+      $('#content').append('<img id="plane" src="plane.svg" />');
+      $('#plane').css({
         'position':'absolute',
         'left':'50%',
         'top':'50%',
         'margin':'auto',
-        'max-width':'100%',
-        'max-height':'100%',
         'overflow':'auto',
-        'transform': 'translate(-50%, -50%)',
-        'font-size': '26pt',
-        'color':'MidnightBlue',
-        'text-decoration':'none',
+        'width':'140px',
+        'transform': 'translate(-180%, -30%)',
+        'pointer-events':'none'
       });
-
-      // When mouse hovers on contact button
-      $('#contact').mouseenter(function() {
-        $('#content').append('<img id="plane" src="plane.svg" />');
-        $('#plane').css({
-          'position':'absolute',
-          'left':'50%',
-          'top':'50%',
-          'margin':'auto',
-          'overflow':'auto',
-          'width':'140px',
-          'transform': 'translate(-180%, -30%)',
-          'pointer-events':'none'
-        });
-        $('#plane').animate({
-          width:'0px',
-          top:'-=2px',
-          left: '+=156px'
-        }, 1600);
-        $(this).css({'text-decoration':'underline'});
-      });
-      // When mouse leaves contact button
-      $('#contact').mouseleave(function() {
-        $(this).css({'text-decoration':'none'});
-        $('#plane').remove();
-      });
+      $('#plane').animate({
+        width:'0px',
+        top:'-=2px',
+        left: '+=156px'
+      }, 1600);
+      $(this).css({'text-decoration':'underline'});
+    });
+    // When mouse leaves contact button
+    $('#contact').mouseleave(function() {
+      $(this).css({'text-decoration':'none'});
+      $('#plane').remove();
     });
   }
 
@@ -595,6 +590,7 @@ $(document).ready(function() {
       analogies();
     }
   }
+
   respond();
 
   $('.homeButton').click(function() {
@@ -621,4 +617,5 @@ $(document).ready(function() {
     console.log('Loading contact page.');
     contact();
   });
+
 });
