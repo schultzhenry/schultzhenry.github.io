1347a18d6cb2a4163f37c10ab8ff1709b80a1247
schultzhenry <natalie.schultz-henry@yale.edu>
2018-08-15 16:43:04 -0400

    nav

--- home.js
+++ home.js
@@ -404,7 +404,7 @@ $(document).ready(function() {
 
     clearPage();
 
-    navBg = 'rgba(230, 230, 250, 0.65)';
+    navBg = 'rgba(230, 230, 250, 0.85)';
     navBgHover = 'rgba(230, 230, 250, 1)';
     navBorder = 'rgba(0, 0, 0, 0)';
     navIconColor = 'rgba(230, 230, 250, 1)';
