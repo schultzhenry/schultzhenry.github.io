2d544e9b6dc806b3270019783a02e4294897551a
schultzhenry <natalie.schultz-henry@yale.edu>
2018-08-24 00:54:33 -0400

    engender

--- home.js
+++ home.js
@@ -589,7 +589,13 @@ $(document).ready(function() {
       'and <a class="engenderLink" ' +
       'href="https://twitter.com/engenderyale">Twitter</a> ' +
       'or see the links below for coverage pertaining ' +
-      'to our efforts.</p>'
+      'to our efforts.</p>' +
+
+      '<p class="engenderText">' +
+      '04.2016\t' +
+      '<a href="https://law.yale.edu/yls-today/news/yale-law-hosts-panel-metoo-movement-0">' +
+      'Yale Law School Panel: Sexual Harassment Law in the Age of Trump and #MeToo</a>' +
+      '</p>'
     );
 
     $('.engenderText').css({
