80694084dcdf02a9914d17ffbbf79d4d6fbd288f
schultzhenry <natalie.schultz-henry@yale.edu>
2018-08-24 00:59:23 -0400

    engender

--- home.js
+++ home.js
@@ -594,7 +594,7 @@ $(document).ready(function() {
       '<p class="engenderText">' +
       '04.2018\t' +
       '<a class="engenderLink" href="https://law.yale.edu/yls-today/news/yale-law-hosts-panel-metoo-movement-0">' +
-      'YLS Panel: Sexual Harassment Law in the Age of Trump and #MeToo</a>' +
+      'Engender participates in YLS panel on sexual harassment law</a>' +
       '</p>'
     );
 
