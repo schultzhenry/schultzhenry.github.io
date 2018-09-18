7bacf63e6f1ae5a363c9ced08ed9062ba18c4483
schultzhenry <natalie.schultz-henry@yale.edu>
2018-09-18 17:17:45 -0400

    file management

--- home.js
+++ home.js
@@ -252,7 +252,7 @@ $(document).ready(function() {
     };
 
     $('#infoText').text(
-      'Thanks for stopping by!'
+      'Thanks for stopping by! Reach me at natalie.schultz-henry@yale.edu'
     );
   };
 
@@ -655,68 +655,6 @@ $(document).ready(function() {
         'flashlight. To try it, Visit this page ' +
         'while using a mouse.</p>');
     }
-  }
-
-  function contact() {
-
-    clearPage();
-
-    $('#content').css({
-      'position': 'fixed',
-      'top': '0px',
-      'left': '0px',
-      'width': '100vw',
-      'height': '100vh',
-      'font-size': '26pt',
-      'color':'navy',
-      'text-align':'center'
-    });
-    $('#content').append(
-      '<a id="contact" ' +
-      'href="mailto:natalie.schultz-henry@yale.edu">' +
-      'Send me a note!' +
-      '</a>');
-    $('#contact').css({
-      'position':'absolute',
-      'left':'50%',
-      'top':'50%',
-      'margin':'auto',
-      'max-width':'100%',
-      'max-height':'100%',
-      'overflow':'auto',
-      'transform': 'translate(-50%, -50%)',
-      'font-size': '26pt',
-      'color':'MidnightBlue',
-      'text-decoration':'none',
-    });
-
-    $('#menu a, #menu a:visited, #info a, #info a:visited').addClass('menuBlack');
-
-    // When mouse hovers on contact button
-    $('#contact').mouseenter(function() {
-      $('#content').append('<img id="plane" src="plane.svg" />');
-      $('#plane').css({
-        'position':'absolute',
-        'left':'50%',
-        'top':'50%',
-        'margin':'auto',
-        'overflow':'auto',
-        'width':'140px',
-        'transform': 'translate(-180%, -30%)',
-        'pointer-events':'none'
-      });
-      $('#plane').animate({
-        width:'0px',
-        top:'-=2px',
-        left: '+=156px'
-      }, 1600);
-      $(this).css({'text-decoration':'underline'});
-    });
-    // When mouse leaves contact button
-    $('#contact').mouseleave(function() {
-      $(this).css({'text-decoration':'none'});
-      $('#plane').remove();
-    });
   }
 
   function respond() {
