19561ea8136ed8d0de2937272a8a91fc621e3f5c
schultzhenry <natalie.schultz-henry@yale.edu>
2018-10-15 17:16:07 -0400

    refresh

--- home.js
+++ home.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
   var today = new Date();
   var day = today.getUTCDate();
   day += (today.getUTCMonth() * 30.4375);
-
-  $(".earth").css('transform', 'rotate(' + day.toString() + 'deg)');
+  $(".earth").css(
+    'transform',
+    'rotate('+day.toString()+'deg)'
+  );
 
   $(".earth").mouseover(function() {
     $( "#earth-label" ).text("today!");
