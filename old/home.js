054e2967a91958b67b0a6a3682c3b18161b1936a
schultzhenry <natalie.schultz-henry@yale.edu>
2019-01-08 18:52:03 -0500

    react

---
+++ old/home.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  var today = new Date();
+  var day = today.getUTCDate();
+  day += (today.getUTCMonth() * 30.4375);
+  $(".earth").css(
+    'transform',
+    'rotate('+day.toString()+'deg)'
+  );
+
+  $(".earth").mouseover(function() {
+    $( "#earth-label" ).text("today!");
+  });
+  $(".earth").mouseout(function() {
+    $( "#earth-label" ).text("");
+  });
+});
