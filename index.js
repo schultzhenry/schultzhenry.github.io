c87b55337f36591b542f9f9d860b7e9141513779
schultzhenry <natalie.schultz-henry@yale.edu>
2021-03-08 11:11:57 -0500

    new format

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
 $(document).ready(function() {
-  if (window.innerHeight < 980) {
-    $(".desktop").hide();
-  } else {
-    $(".desktop").show();
-  }
+  // if (window.innerHeight < 980) {
+  //   $(".desktop").hide();
+  // } else {
+  //   $(".desktop").show();
+  // }
 
-  window.onresize = function () {
-    if (window.innerHeight < 980) {
-      $(".desktop").hide();
-    } else {
-      $(".desktop").show();
-    }
-  };
+  // window.onresize = function () {
+  //   if (window.innerHeight < 980) {
+  //     $(".desktop").hide();
+  //   } else {
+  //     $(".desktop").show();
+  //   }
+  // };
 
-  $.scrollify({
-		section:".view",
-    scrollbars:false,
-    after:function(i,panels) {
-      $(".view").each(function(i) {
-        activeClass = "";
-        console.log($.scrollify.currentIndex());
-        if(i===$.scrollify.currentIndex()) {
-          activeClass = "active";
-        }
-        $("#nav"+i).attr("class", activeClass);
-        $(this).attr("class", "view " + activeClass);
-      })
-    }
-  });
+  // $.scrollify({
+	// 	section:".view",
+  //   scrollbars:false,
+  //   after:function(i,panels) {
+  //     $(".view").each(function(i) {
+  //       activeClass = "";
+  //       console.log($.scrollify.currentIndex());
+  //       if(i===$.scrollify.currentIndex()) {
+  //         activeClass = "active";
+  //       }
+  //       $("#nav"+i).attr("class", activeClass);
+  //       $(this).attr("class", "view " + activeClass);
+  //     })
+  //   }
+  // });
 
-  var sectionLabels = ["Home", "116th", "Engender", "Duby Team", "Fuzzy Grids II", "About"];
-
-  $(".nav div").each(function(i) {
-    $(this).on("click touchstart", function() {
-      $.scrollify.move(i);
-    }).on("mouseover", function () {
-      $("#pnav" + i).text( sectionLabels[i] );
-    }).on("mouseleave", function () {
-      $("#pnav" + i).text( "" );
-    });
-  });
+  // var sectionLabels = [
+  //   "Home",
+  //   "Mayor Brandon M. Scott's First 100 Days Tracker",
+  //   "Baltimore City COVID-19 Dashboard",
+  //   "Federal Judiciary Interactive",
+  //   "116th",
+  //   "Engender",
+  //   "Duby Team",
+  //   "Fuzzy Grids II",
+  //   "About"];
+  //
+  // $(".nav div").each(function(i) {
+  //   $(this).on("click touchstart", function() {
+  //     $.scrollify.move(i);
+  //   }).on("mouseover", function () {
+  //     $("#pnav" + i).text( sectionLabels[i] );
+  //   }).on("mouseleave", function () {
+  //     $("#pnav" + i).text( "" );
+  //   });
+  // });
 
   function Dec(num, total) {
     return Math.abs((num - 1) % total);
@@ -83,6 +92,5 @@ $(document).ready(function() {
     ZineList = Inc(ZineList, ZineTotal);
     $("#ZineImage").attr("src", "zine-116/zine-116-" + ((ZineList < 10) ? ("0" + String(ZineList)) : String(ZineList)) + ".png");
   });
-
 
 });
